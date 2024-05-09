50ac1f166574b4c776f363320cff98603c520ad7
tarari <t.jimenez@escolesnuria.cat>
2024-05-09 18:58:54 +0200

    Nav and login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -31,7 +31,7 @@ class AuthController extends Controller
             $user = json_decode($response)->content->name;
 
             $token = $response->json('token');
-            dd($response);
+
             session(['name' => $user]);
             session(['token' => $token]);
             session()->save();
