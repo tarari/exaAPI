41057e2bea4f6919f0ea16363c08e12ab9a6c3c0
tarari <t.jimenez@escolesnuria.cat>
2024-05-10 17:47:31 +0200

    overflow-y footer

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,13 +7,13 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <title>API.proof</title>
 </head>
-<body class="flex flex-col h-screen justify-between">
+<body class="flex flex-col h-screen justify-between h-screen overflow-hidden">
     <header class="h-10 bg-gray-500 text-center  text-white text-3xl">ApiProof v.1.0</header>
     <x-nav>{{ $slot }}</x-nav>
-    <main class="mb-auto h-10">
+    <main class="mb-auto h-10 overflow-y-scroll">
         {{ $slot }}
     </main>
 
-    <footer class="sticky top-[100vh] bg-black text-white px-auto py-4 text-center">API.Proof - by Toni Jimenez, 2024</footer>
+    <footer class="w-full bg-black text-white px-auto py-4 text-center">API.Proof - by Toni Jimenez, 2024</footer>
 </body>
 </html>
