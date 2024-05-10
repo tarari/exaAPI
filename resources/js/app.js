1273f009582904ca025cea4b5edd4632e8e954df
tarari <t.jimenez@escolesnuria.cat>
2024-05-10 18:19:56 +0200

    foto

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,6 @@
 
 import './bootstrap';
+import Alpine from 'alpinejs';
+window.Alpine = Alpine;
 
+Alpine.start();
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,7 +8,9 @@
     <title>API.proof</title>
 </head>
 <body class="flex flex-col h-screen justify-between h-screen overflow-hidden">
-    <header class="h-10 bg-gray-500 text-center  text-white text-3xl">ApiProof v.1.0</header>
+    <header class="h-10 bg-gray-500 text-center  text-white text-3xl">
+        <img class="w-2 h-2" src="{{asset('storage/toni-boda.png')}}" alt="API.proof">
+        ApiProof v.1.0</header>
     <x-nav>{{ $slot }}</x-nav>
     <main class="mb-auto overflow-y-scroll">
         {{ $slot }}
