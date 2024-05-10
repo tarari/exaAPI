33bd4d368cf614f1c1930f353529ab89b3eeb467
tarari <t.jimenez@escolesnuria.cat>
2024-05-10 15:32:26 +0200

    results component

--- resources/views/components/results.blade.php
+++ resources/views/components/results.blade.php
@@ -1,5 +1,5 @@
 <div class="flex flex-col items-center justify-center bg-blue-100">
     @foreach ($data as $item)
-    <code>{ print_r($item}</code>
+    <code> {{ print_r($item) }} </code>
     @endforeach
 </div>
