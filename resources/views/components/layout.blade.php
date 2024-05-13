a22895f515ee9427f98f658622b0414878dcbfd0
tarari <t.jimenez@escolesnuria.cat>
2024-05-13 16:38:15 +0200

    foto

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,7 +9,7 @@
 </head>
 <body class="flex flex-col h-screen justify-between h-screen overflow-hidden">
     <header class="h-10 bg-gray-500 text-center  text-white text-3xl m-2">
-        <img class="w-2 h-2 rounded-full" src="{{asset('img/toni-boda.png')}}" alt="API.proof">
+        <a href="https://toniprojects.eu"><img class="w-2 h-2 rounded-full" src="{{asset('img/toni-boda.png')}}" alt="API.proof"></a>
         ApiProof v.1.0</header>
     <x-nav>{{ $slot }}</x-nav>
     <main class="mb-auto overflow-y-scroll">
