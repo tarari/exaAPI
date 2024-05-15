4a9fc84cf785b759494b7696beea15291b3271bf
tarari <t.jimenez@escolesnuria.cat>
2024-05-15 17:00:42 +0200

    search form undo

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -14,7 +14,7 @@
         <div class="px-4"><h1 class="flex-inline text-2xl">ApiProof v.1.0</h1></div></header>
     <x-nav>{{ $slot }}</x-nav>
     <div id="search" class="flex flex-col items-center justify-center py-4 w-full">
-        <form action="/search" class="flex flex-inline w-full items-center justify-center" smethod="GET">
+        <form action="#" class="flex flex-inline w-full items-center justify-center" method="POST">
             @csrf
             <input class="m-2 p-2 border-2 rounded-md w-1/2" type="text" name="category_id" placeholder="Search...by Category">
             <input type="submit"  class="m-2 bg-blue-500 text-white px-4 py-2 rounded-md" value="Search">
